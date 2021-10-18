5d4a2c98154efe4bf1641a425b5d6d35c22d2421
이숙영 <22sook00@gmail.com>
2021-10-18 12:53:33 +0900

    [해시] 완주하지못한 선수

--- 완주하지못한선수.js
+++ 완주하지못한선수.js
@@ -1,6 +1,7 @@
 function solution(participant, completion) {
   // 항상 참가자가 하나 더많고 비교대상이 완주한사람이 됨.
-  // 알파벳 순으로 sorting 처리해서 같은 인덱스끼리 비교가능하도록 만든다.
+  // 알파벳 순으로 sorting 처리
+  // 같은 인덱스끼리 비교가능하도록 만든다.
   // 참가자 for 문 돌린다음에 i번째 비교하기
   
   participant.sort();
