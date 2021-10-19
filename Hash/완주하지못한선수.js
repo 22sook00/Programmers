3690df82d62224b27a0c8d143dce932215bf940a
이숙영 <22sook00@gmail.com>
2021-10-19 14:57:57 +0900

    [프로그래머스 1단계] 완주하지못한 선수

--- Hash/완주하지못한선수.js
+++ Hash/완주하지못한선수.js
@@ -1,3 +1,10 @@
+/*
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 
+단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+*/
+
 function solution(participant, completion) {
   // 항상 참가자가 하나 더많고 비교대상이 완주한사람이 됨.
   // 알파벳 순으로 sorting 처리
@@ -13,4 +20,15 @@ function solution(participant, completion) {
       }
   }
   
+}
+
+//better || another code
+const solution = (participant, completion) => {
+    participant.sort()
+    completion.sort()
+    while (participant.length) {
+        //pop 을 해주므로서 participant.length 를 0 으로 만들어주고 무한루프를 방지할 수 있다.  
+        let result = participant.pop()
+        if (result !== completion.pop()) return result
+    }
 }
