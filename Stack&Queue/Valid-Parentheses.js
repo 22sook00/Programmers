a1e7a33a631f80f3306f7806b7a89304672db176
이숙영 <22sook00@gmail.com>
2021-11-05 01:20:15 +0900

    [LeetCode] 유효한괄호

---
+++ Stack&Queue/Valid-Parentheses.js
@@ -0,0 +1,43 @@
+/*
+Given a string s containing just the characters 
+'(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+*/
+var isValid = function(s) {
+
+//스택활용문제
+  const temp = {
+      '(' : ')',
+      '{' : '}',
+      '[' : ']'
+  };
+  const stack = [];
+  const keys = Object.keys(temp);
+  for (let i = 0; i< s.length; i++) {
+      if(keys.includes(s[i])) {
+          stack.push(s[i]);
+      }
+      else {
+          if (temp[stack[stack.length-1]] === s[i]) {
+              stack.pop();
+          }
+          else {
+              return false;
+          }
+      }
+  };      
+  return !stack.length;
+}
